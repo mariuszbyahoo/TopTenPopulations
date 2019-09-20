8dae0362a6453678435586c5f0f644f45e2ad1bb
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-09-20 14:19:23 +0200

    Populating the Array with a For loop

--- TopTenPopulations/CsvReader.cs
+++ TopTenPopulations/CsvReader.cs
@@ -1,20 +1,41 @@
 ﻿using System;
+using System.IO;
 
 namespace TopTenPopulations
 {
     public class CsvReader
     {
-        private string filePath;
+        private string _csvFilePath;
 
         public CsvReader(string filePath)
         {
-            this.filePath = filePath;
+            this._csvFilePath = filePath;
         }
 
         public Country[] ReadFirstNCountries(int nCountries)
         {
+
 // Instantiating an array without knowing its elements
+
             Country [] countries = new Country[nCountries];
+
+/* using statement is required by StreamReader and makes sure that the StreamReader
+ object is disposed of once we've finished with it. 
+ It's important, because the StreamReaders actually lock the file they're reading,
+ and disposing the StreamReader will unlock the file so other processes can use it.*/
+
+            using (StreamReader sr = new StreamReader(_csvFilePath))
+            {
+                // read header line, it needs to be ignored.
+                sr.ReadLine();
+
+                for(int i = 0; i < nCountries; i++)
+                {
+                    string csvLine = sr.ReadLine();
+                    countries[i] = ReadCountryFromCsvLine(csvLine);
+                }
+            }
+
             return countries;
         }
 
