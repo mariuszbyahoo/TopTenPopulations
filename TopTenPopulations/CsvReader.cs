4de63ccb23231583e493f236d51e52b8b305b5d4
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-09-20 13:51:05 +0200

    Initializing a demo sample: Importing Countries

---
+++ TopTenPopulations/CsvReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TopTenPopulations
+{
+    public class CsvReader
+    {
+        private string filePath;
+
+        public CsvReader(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public Country[] ReadFirstNCountries(int nCountries)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TopTenPopulations/Program.cs
+++ TopTenPopulations/Program.cs
@@ -6,7 +6,15 @@ namespace TopTenPopulations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string filePath = @"A:\data\Pop by Largest Final.csv";
+            CsvReader reader = new CsvReader(filePath);
+
+            Country[] countries = reader.ReadFirstNCountries(10);
+
+            foreach (Country country in countries)
+            {
+                Console.WriteLine($"{country.Population} : {country.Name}");
+            }
         }
     }
 }
