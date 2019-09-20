044cd5f9bc161e9d872c7aa04300cec664e0106f
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-09-20 21:30:05 +0200

    Searching with List<T>.FindIndex()

--- TopTenPopulations/Program.cs
+++ TopTenPopulations/Program.cs
@@ -11,7 +11,9 @@ namespace AllCountriesPopulation
             CsvReader reader = new CsvReader(filePath);
 
             List<Country> countries = reader.ReadAllCountries();
-
+            Country lilliput = new Country("Lilliput", "LIL", "Somewhere", 2_000_000);
+            int lilliputIndex = countries.FindIndex(x => x.Population < 2_000_000);
+            countries.Insert(lilliputIndex, lilliput);
             foreach (Country country in countries)
             {
                 Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)} : { country.Name}");
