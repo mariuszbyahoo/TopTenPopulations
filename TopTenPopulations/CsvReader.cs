78521bc3d577eb1895d7e8f1c0ac2345fb0cd095
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-09-20 21:20:48 +0200

    Counting elements with Array.Length

--- TopTenPopulations/CsvReader.cs
+++ TopTenPopulations/CsvReader.cs
@@ -33,23 +33,41 @@ namespace AllCountriesPopulation
                 string csvLine;
                 while ((csvLine = sr.ReadLine()) != null)
                 { 
-                    csvLine = sr.ReadLine();
                     countries.Add(ReadCountryFromCsvLine(csvLine));
                 }
             }
-
             return countries;
         }
 
         public Country ReadCountryFromCsvLine(string csvLine)
         {
-            string[] parts = csvLine.Split(new char[] { ',' });
+            Console.WriteLine(csvLine);
+            string[] parts = csvLine.Split(',');
 
-            string name = parts[0];
-            string code = parts[1];
-            string region = parts[2];
-            int population = int.Parse(parts[3]);
-
+            string name;
+            string code;
+            string region;
+            string popText;
+            switch (parts.Length)
+            {
+                case 4:
+                    name = parts[0];
+                    code = parts[1];
+                    region = parts[2];
+                    popText = parts[3];
+                    break;
+                case 5:
+                    name = parts[0] + ", " + parts[1];
+                    name = name.Replace("\"", null).Trim();
+                    code = parts[2];
+                    region = parts[3];
+                    popText = parts[4];
+                    break;
+                default:
+                    throw new Exception($"Can't parse country from csvLine: {csvLine}");
+            }
+            // TryParse leaves population=0 if can't parse
+            int.TryParse(popText, out int population);
             return new Country(name, code, region, population);
         }
     }
