cf3b2548814b303d4e41b580fc29481021776a7c
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-09-20 14:06:39 +0200

    Parsing a CSV Line with String.Split()

--- TopTenPopulations/CsvReader.cs
+++ TopTenPopulations/CsvReader.cs
@@ -17,5 +17,17 @@ namespace TopTenPopulations
             Country [] countries = new Country[nCountries];
             return countries;
         }
+
+        public Country ReadCountryFromCsvLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(',');
+
+            string name = parts[0];
+            string code = parts[1];
+            string region = parts[2];
+            int population = int.Parse(parts[3]);
+
+            return new Country(name, code, region, population);
+        }
     }
 }
