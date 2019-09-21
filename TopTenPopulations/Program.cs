f0a5feb9ef5cc0e4b68ebb8b2b78680c979bcaad
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-09-21 20:29:14 +0200

    Chaining LINQ methods

--- TopTenPopulations/Program.cs
+++ TopTenPopulations/Program.cs
@@ -13,10 +13,11 @@ namespace AllCountriesPopulation
 
             List<Country> countries = reader.ReadAllCountries();
             
-// The OrderBy() method requires a delegate or lambda expression passed 
-// in as an argument of the method.
-            foreach (Country country in countries.OrderBy(x => x.Name))
-// LINQ doesn't really support batching
+/* The OrderBy() method requires a delegate or lambda expression passed 
+* in as an argument of the method. */
+            foreach (Country country in countries.OrderBy(x => x.Name).Take(10))
+/* LINQ doesn't really support batching, but, it supports method chaining anyway.
+Be aware, because the results depend on the order in wich you are chaining the operations in*/
             {
                 Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)} : { country.Name}");
             }
