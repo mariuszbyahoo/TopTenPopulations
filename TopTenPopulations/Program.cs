5dfc3ae34fa371a640226d66034c3152db5b25ee
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-09-21 21:00:38 +0200

    LINQ query syntax

--- TopTenPopulations/Program.cs
+++ TopTenPopulations/Program.cs
@@ -13,16 +13,26 @@ namespace AllCountriesPopulation
 
             List<Country> countries = reader.ReadAllCountries();
 
-/* The OrderBy() method requires a delegate or lambda expression passed 
-* in as an argument of the method. 
-* LINQ doesn't really support batching, but, it supports method chaining anyway.
+            /* The OrderBy() method requires a delegate or lambda expression passed 
+            * in as an argument of the method. 
+            * LINQ doesn't really support batching, but, it supports method chaining anyway.
 
-* Be aware, because the results depend on the order in wich you are chaining the operations in
+            * Be aware, because the results depend on the order in wich you are chaining the operations in
 
-* Moreover, LINQ cannot remove any data from a data source, according to the fact, that it is
-* designed only to recieve items from the Data Source and pass them on. */
+            * Moreover, LINQ cannot remove any data from a data source, according to the fact, that it is
+            * designed only to recieve items from the Data Source and pass them on. */
 
-            foreach (Country country in countries.Where(x => !x.Name.Contains(',')).Take(20))
+            var filteredCountries = countries.Where(x => !x.Name.Contains(','));//.Take(20);
+
+// The same query below uses the LINQ Query Syntax: it tries to mimic SQL
+// but still, is working in the same way. Actually, it just doesn't support 
+// all of the LINQ features.
+
+            var filteredCountries2 = from country in countries
+                                     where !country.Name.Contains(',')
+                                     select country;
+
+            foreach (Country country in filteredCountries2)
             {
                 Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)} : { country.Name}");
             }
